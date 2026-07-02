1a7160a14e0d6590275a85d25c04ea193cc93e06
CyCklop <ignacioandresflores06@gmail.com>
2026-07-02 11:27:54 -0400

    modificado el config

--- src/main/java/cl/esperanza/pago/config/WebClientConfig.java
+++ src/main/java/cl/esperanza/pago/config/WebClientConfig.java
@@ -3,12 +3,13 @@ package cl.esperanza.pago.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient facturacionWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8083/api/v1/facturacion").build();
+    public WebClient facturacionWebClient(@Value("${facturacion.service.url:http://localhost:8083/api/v1/facturacion}") String facturacionServiceUrl) {
+        return WebClient.builder().baseUrl(facturacionServiceUrl).build();
     }
 }
